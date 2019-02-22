0543715be8ceebb7ef0621377b7f20978ce1272d
rezatrue <rezatrue@gmail.com>
2019-02-23 00:44:56 +0600

    keyword listener added

--- src/application/MainController.java
+++ src/application/MainController.java
@@ -3,6 +3,9 @@ package application;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import org.jnativehook.GlobalScreen;
+import org.jnativehook.NativeHookException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -13,15 +16,44 @@ public class MainController implements Initializable{
 	@FXML
 	private Button startBtn;
 
+	boolean status = true;
+	
+	GlobalKeyListener globalKeyListener;
+	
 	@FXML
 	public void startBtnAction(ActionEvent event) {
+		
 		System.out.println("Button is clicked");
+		if(status) {
+			GlobalScreen.addNativeKeyListener(globalKeyListener);
+			status = false;
+		}
+		else {
+			GlobalScreen.removeNativeKeyListener(globalKeyListener);
+			status = true;
+		}
+
 	}
 	
 	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		// TODO Auto-generated method stub
+		
+		// http://www.iamkarthi.com/tutorial-jnativehook-control-native-mouse-and-keyboard-calls-outside-java/
+		globalKeyListener = new GlobalKeyListener();
+
+		try {
+			GlobalScreen.registerNativeHook();
+			}
+			catch (NativeHookException ex) {
+			System.err.println("There was a problem registering the native hook.");
+			System.err.println(ex.getMessage());
+
+			System.exit(1);
+			}
+
+			
+			
 		
 	}
 
